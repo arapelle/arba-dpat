75a8744d81c2cf1bb58535a8aff1aeb437303525
Aymeric Pellé <aympelle@gmail.com>
2025-06-17 08:49:15 +0200

    v0.1.0:#3: Adds `under` class for hierarchy management.
    Introduces the `under` class template, enabling type-safe management
    of object hierarchies. The class provides access to superior objects
    in the hierarchy, with options for optional or substitutable superior
    relationships.

---
+++ include/arba/dpat/under/under.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <type_traits>
+#include <cassert>
+
+inline namespace arba
+{
+namespace dpat
+{
+
+struct hierarchy_superior_traits
+{
+    enum
+    {
+        optional = 1,
+        substiuable = 2
+    };
+};
+
+template <class SuperiorType, unsigned SuperiorTraits = 0>
+class under
+{
+protected:
+    under()
+        requires ((SuperiorTraits & hierarchy_superior_traits::optional) > 0)
+    {}
+
+    explicit under(SuperiorType& superior)
+        : superior_ptr_(&superior)
+    {
+        assert(&superior);
+    }
+
+    [[nodiscard]] inline bool has_superior() const noexcept
+    {
+        if constexpr ((SuperiorTraits & hierarchy_superior_traits::optional) == 0)
+            return true;
+        else
+            return superior_ptr_;
+    }
+
+    [[nodiscard]] inline const SuperiorType& superior() const noexcept { assert(has_superior()); return *superior_ptr_; }
+    [[nodiscard]] inline SuperiorType& superior() noexcept { assert(has_superior()); return *superior_ptr_; }
+
+    inline void set_superior(SuperiorType& superior) noexcept requires (SuperiorTraits > 0)
+    {
+        assert(&superior);
+        superior_ptr_ = &superior;
+    }
+
+    template <class RequestedType>
+    [[nodiscard]] inline const RequestedType& superior() const
+    {
+        if constexpr (std::is_same_v<SuperiorType, RequestedType>)
+        {
+            return this->superior();
+        }
+        else
+        {
+            return this->superior().template superior<RequestedType>();
+        }
+    }
+
+    template <class RequestedType>
+    [[nodiscard]] inline RequestedType& superior()
+    {
+        if constexpr (std::is_same_v<SuperiorType, RequestedType>)
+        {
+            return this->superior();
+        }
+        else
+        {
+            return this->superior().template superior<RequestedType>();
+        }
+    }
+
+    template <class, unsigned>
+    friend class under;
+
+private:
+    SuperiorType* superior_ptr_ = nullptr;
+};
+
+}
+}
---
+++ test/under/under_optional_tests.cpp
@@ -0,0 +1,110 @@
+#include <arba/dpat/under/under.hpp>
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+namespace ut
+{
+
+constexpr unsigned hierarchy_superior_traits = dpat::hierarchy_superior_traits::optional;
+
+class application;
+class graphics_engine;
+class scene_node;
+
+class scene_node : public dpat::under<graphics_engine, hierarchy_superior_traits>
+{
+public:
+    explicit scene_node(graphics_engine& gr_eng) : dpat::under<graphics_engine, hierarchy_superior_traits>(gr_eng) {}
+    scene_node() : dpat::under<graphics_engine, hierarchy_superior_traits>() {}
+
+    using dpat::under<graphics_engine, hierarchy_superior_traits>::has_superior;
+    using dpat::under<graphics_engine, hierarchy_superior_traits>::set_superior;
+
+    [[nodiscard]] inline const graphics_engine& engine() const { return this->superior(); }
+    [[nodiscard]] inline       graphics_engine& engine()       { return this->superior(); }
+
+    [[nodiscard]] inline const application& app() const { return this->superior<application>(); }
+    [[nodiscard]] inline       application& app()       { return this->superior<application>(); }
+};
+
+class graphics_engine : public dpat::under<application, hierarchy_superior_traits>
+{
+public:
+    explicit graphics_engine(application& app) : dpat::under<application, hierarchy_superior_traits>(app), root_scene_node(*this) {}
+
+    using dpat::under<application, hierarchy_superior_traits>::has_superior;
+    using dpat::under<application, hierarchy_superior_traits>::set_superior;
+
+    [[nodiscard]] inline const application& app() const { return this->superior(); }
+    [[nodiscard]] inline       application& app()       { return this->superior(); }
+
+    void set_node()
+    {
+        scene_node node;
+        node.set_superior(*this);
+        root_scene_node = node;
+    }
+
+    scene_node root_scene_node;
+};
+
+class application
+{
+public:
+    explicit application() : gr_engine_uptr(std::make_unique<graphics_engine>(*this)) {}
+
+    std::unique_ptr<graphics_engine> gr_engine_uptr;
+};
+
+} // namespace ut
+
+TEST(under_optional_tests, superior__direct_superior__ok)
+{
+    ut::application app;
+    ASSERT_EQ(&app, &app.gr_engine_uptr->app());
+    ASSERT_EQ(app.gr_engine_uptr.get(), &app.gr_engine_uptr->root_scene_node.engine());
+}
+
+TEST(under_optional_tests, const_superior__direct_superior__ok)
+{
+    const ut::application app;
+    ASSERT_EQ(&app, &app.gr_engine_uptr->app());
+    ASSERT_EQ(app.gr_engine_uptr.get(), &app.gr_engine_uptr->root_scene_node.engine());
+}
+
+TEST(under_optional_tests, superior__indirect_superior__ok)
+{
+    ut::application app;
+    ASSERT_EQ(&app, &app.gr_engine_uptr->root_scene_node.app());
+}
+
+TEST(under_optional_tests, const_superior__indirect_superior__ok)
+{
+    const ut::application app;
+    ASSERT_EQ(&app, &app.gr_engine_uptr->root_scene_node.app());
+}
+
+TEST(under_optional_tests, has_superior__ok)
+{
+    const ut::application app;
+    ASSERT_TRUE(app.gr_engine_uptr->has_superior());
+    ASSERT_TRUE(app.gr_engine_uptr->root_scene_node.has_superior());
+}
+
+TEST(under_optional_tests, set_superior__ok)
+{
+    ut::application app;
+    ut::application new_app;
+    new_app.gr_engine_uptr = std::move(app.gr_engine_uptr);
+    new_app.gr_engine_uptr->set_superior(new_app);
+    ASSERT_EQ(&new_app, &new_app.gr_engine_uptr->app());
+}
+
+TEST(under_optional_tests, default_constructor__no_superior__ok)
+{
+    ut::application app;
+    app.gr_engine_uptr->set_node();
+    ASSERT_EQ(app.gr_engine_uptr.get(), &app.gr_engine_uptr->root_scene_node.engine());
+}
